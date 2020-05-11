8f3aada23269cbd49ad1f637f463f2318fc762a8
ordchr <mongoostic@yandex.ru>
2020-05-11 23:29:42 +0800

    Используем форму при отправке данных

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -2,6 +2,7 @@ let editButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__button-close');
 let popupSaveButton = document.querySelector('.popup__button-save');
 let popup = document.querySelector('.popup');
+let editProfileForm = document.querySelector('.popup__form');
 let profileFullName = document.querySelector('.profile__full-name');
 let profileProfession = document.querySelector('.profile__profession');
 let popupFullNameValue = document.querySelector('.popup__full-name');
@@ -21,13 +22,13 @@ function closeEditProfilePopup() {
   popup.classList.remove('popup_opened');
 }
 
-function SaveProfile() {
+function formSubmitHandler (evt) {
+  evt.preventDefault();
   profileFullName.textContent = popupFullNameValue.value;
   profileProfession.textContent = popupProfessionValue.value;
   closeEditProfilePopup();
 }
 
 editButton.addEventListener('click', showEditProfilePopup);
-popupSaveButton.addEventListener('click', SaveProfile);
 popupCloseButton.addEventListener('click', closeEditProfilePopup);
-
+editProfileForm.addEventListener('submit', formSubmitHandler);
