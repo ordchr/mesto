c8af4001fa3390f7d351b88467498ebc02d3e88d
Антон <mongoostic@yandex.ru>
2020-05-18 23:06:04 +0800

    Исправления по замечаниям

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,16 +1,15 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = document.querySelector('.popup__button-close');
-let popupSaveButton = document.querySelector('.popup__button-save');
-let popup = document.querySelector('.popup');
-let editProfileForm = document.querySelector('.popup__form');
-let profileFullName = document.querySelector('.profile__full-name');
-let profileProfession = document.querySelector('.profile__profession');
-let popupFullNameValue = document.querySelector('.popup__full-name');
-let popupProfessionValue = document.querySelector('.popup__profession');
+const editButton           = document.querySelector('.profile__edit-button');
+const popupCloseButton     = document.querySelector('.popup__button-close');
+const popup                = document.querySelector('.popup');
+const editProfileForm      = document.querySelector('.popup__form');
+const profileFullName      = document.querySelector('.profile__full-name');
+const profileProfession    = document.querySelector('.profile__profession');
+const popupFullNameValue   = document.querySelector('.popup__input_full-name');
+const popupProfessionValue = document.querySelector('.popup__input_profession');
 
 
 function showEditProfilePopup() {
-  popupFullNameValue.value = profileFullName.textContent;
+  popupFullNameValue.value   = profileFullName.textContent;
   popupProfessionValue.value = profileProfession.textContent;
   popup.classList.remove('popup_closed');
   popup.classList.add('popup_opened');
@@ -24,7 +23,7 @@ function closeEditProfilePopup() {
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  profileFullName.textContent = popupFullNameValue.value;
+  profileFullName.textContent   = popupFullNameValue.value;
   profileProfession.textContent = popupProfessionValue.value;
   closeEditProfilePopup();
 }
