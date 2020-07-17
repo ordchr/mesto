c5ffcbae14c9359bdacb1f03f1d8e0ac02e66aa1
Антон <mongoostic@yandex.ru>
2020-07-17 11:28:44 +0800

    Рефакторинг - выносим в отдельный файл данные для создания карточек

--- script/index.js
+++ script/index.js
@@ -80,32 +80,6 @@ places.addEventListener('click', (evt) => {
   }
 });
 
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
 
 const showPopupForm = (targetPopup, submitHandler) => {
   const popupForm = targetPopup.querySelector('.popup__form');
