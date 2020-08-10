6e1e0b1ee992884e7cd13a0a1f4b8e2fe2174672
Антон <mongoostic@yandex.ru>
2020-08-10 21:26:53 +0800

    Настройка webpack - поддержка изображений, шрифтов, css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
@@ -12,6 +14,23 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        loader: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff2)$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        // options: {
+          // url(url) {
+            // return url !== '';
+          // }
+        // }
       }
     ]
 
@@ -19,6 +38,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin()
   ],
+  devServer: {
+    host: '0.0.0.0',
+    port: 8000,
+    disableHostCheck: true
+  }
 }
