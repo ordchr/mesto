ee28356e190f1ca74606981bf6fd699395f1187a
Антон <mongoostic@yandex.ru>
2020-08-11 16:19:46 +0800

    Настраиваем обратно работу с gh pages

---
+++ src/index_copy.js
@@ -0,0 +1,85 @@
+import { Card } from './components/Card.js';
+import { Section } from './components/Section.js';
+import { PopupWithForm } from './components/PopupWithForm.js';
+import { PopupWithImage } from './components/PopupWithImage.js';
+import { FormValidator } from './components/FormValidator.js';
+import { UserInfo } from './components/UserInfo.js';
+import { initialCards } from './initial-cards.js';
+
+const editButton = document.querySelector('.profile__edit-button');
+const addCardButton = document.querySelector('.profile__add-button');
+
+const popupFormCardAdd = document.querySelector('.popup__form-card-add');
+const popupFormProfileEdit = document.querySelector('.popup__form-edit_profile');
+const popupFullNameValue = document.querySelector('.popup__input_full-name');
+const popupProfessionValue = document.querySelector('.popup__input_profession');
+
+// Форма для добавления карточки
+const popupInputPlaceName = document.querySelector('.popup__input_place-name');
+const popupInputImageLink = document.querySelector('.popup__input_place-image-link');
+
+const validateOptions = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button-save',
+  inactiveButtonClass: 'popup__button-save_inactive',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active'
+};
+
+// Установка слушателей для валидации форм
+const formCardAddValidator = new FormValidator(validateOptions, popupFormCardAdd);
+formCardAddValidator.enableValidation();
+const formProfileEditValidator = new FormValidator(validateOptions, popupFormProfileEdit);
+formProfileEditValidator.enableValidation();
+
+const popupPreview = new PopupWithImage('.popup-preview');
+popupPreview.setEventListeners();
+
+const cardRenderer = (placeName, placeLink) => {
+  const card = new Card(placeName, placeLink, '#place', (item) => {
+    popupPreview.open(item);
+  });
+  return card.getCard();
+}
+
+const section = new Section(
+  {
+    items: initialCards,
+    renderer: cardRenderer,
+  },
+  '.places'
+);
+
+section.renderAll();
+
+const formAddCardSubmitHandler = () => {
+  section.addItem(cardRenderer(popupInputPlaceName.value, popupInputImageLink.value));
+}
+
+const userInfo = new UserInfo({
+  userNameSelector: '.profile__full-name',
+  userInfoSelector: '.profile__profession'
+});
+
+const formSubmitHandler = (inputValues) => {
+  userInfo.setUserInfo(inputValues);
+}
+
+const popupProfileEdit = new PopupWithForm('.popup_profile', formSubmitHandler);
+popupProfileEdit.setEventListeners();
+
+editButton.addEventListener('click', () => {
+  popupFullNameValue.value   = userInfo.getUserInfo().userName;
+  popupProfessionValue.value = userInfo.getUserInfo().userInfo;
+  popupProfileEdit.open();
+});
+
+const popupAddCard = new PopupWithForm('.popup_add-card', formAddCardSubmitHandler);
+popupAddCard.setEventListeners();
+
+addCardButton.addEventListener('click', () => {
+  popupAddCard.open();
+});
+
+
